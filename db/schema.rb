2e540384f477ef556d9b5a62bdc13c74ed4d35ba
Dustin Burton <dustin.a.burton@gmail.com>
2015-09-18 17:29:16 +0000

    removed duplicate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150918150632) do
-=======
-ActiveRecord::Schema.define(version: 20150918155539) do
->>>>>>> development
 
   create_table "contacts", force: true do |t|
     t.string   "name"
